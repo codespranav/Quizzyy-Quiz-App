840d4fca03daac31b7598dbd4ba0dc428a6847b6
Pranav <pranavk24h@gmail.com>
2024-09-04 14:20:07 +0530

    add header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+
 
 const Home = () => {
   return (
-    <div>
-        <h1>Hello</h1>
+    <div className=''>
+        <Navbar/>
     </div>
   )
 }
